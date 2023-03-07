219e9904b1cf2903d3b12496e74fd1ebe6687515
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2023-03-07 09:47:25 +0100

    setup.py: bugfix in data paths

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 from glob import glob
+import os
+
+filepath = os.path.dirname(os.path.abspath(__file__))
+
 
 visu_mesh_data = glob("eve/visualisation/meshes/*")
-organic3dv2_data = glob("eve/vesseltree/data/cad/organic3dv2")
-organicv2_data = glob("eve/vesseltree/data/cad/organicv2")
+organic3dv2_data = glob("eve/vesseltree/data/cad/organic3dv2/*")
+organicv2_data = glob("eve/vesseltree/data/cad/organicv2/*")
 setup(
     name="eve",
     version="0.2dev",
