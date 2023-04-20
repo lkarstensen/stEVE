00932d42239fed3ab7b068a0cf5d0f55606190a0
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2023-04-20 21:23:59 +0200

    env: moved dummyenv to eve_rl

--- eve/env.py
+++ eve/env.py
@@ -228,30 +228,3 @@ class EnvObsInfoOnly(Env):
             if value["_id"] in reduced_object_list.keys():
                 new_config_dict[entry] = value
         return new_config_dict
-
-
-class DummyEnv(Env):
-    def __init__(  # pylint: disable=super-init-not-called
-        self, *args, **kwds  # pylint: disable=unused-argument
-    ) -> None:
-        ...
-
-    @property
-    def observation_space(self) -> gym.Space:
-        return gym.spaces.Box(low=np.empty((1,)), high=np.empty((1,)))
-
-    @property
-    def action_space(self) -> gym.Space:
-        return gym.spaces.Box(low=np.empty((1,)), high=np.empty((1,)))
-
-    def step(self, action: np.ndarray) -> None:
-        ...
-
-    def reset(self, *args, **kwds) -> None:  # pylint: disable=unused-argument
-        ...
-
-    def render(self) -> None:
-        ...
-
-    def close(self) -> None:
-        ...
