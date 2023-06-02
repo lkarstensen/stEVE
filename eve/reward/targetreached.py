2d4b7170114319664b24b88112c1106c431abdbc
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2023-06-02 08:44:31 +0200

    reward.targetreached: included interimtarget

--- eve/reward/targetreached.py
+++ eve/reward/targetreached.py
@@ -1,14 +1,23 @@
+from typing import Optional
 from .reward import Reward
 from ..intervention import Intervention
+from ..interimtarget import InterimTarget, InterimTargetDummy
 
 
 class TargetReached(Reward):
-    def __init__(self, intervention: Intervention, factor: float) -> None:
+    def __init__(
+        self,
+        intervention: Intervention,
+        factor: float,
+        interim_target: Optional[InterimTarget] = None,
+    ) -> None:
         self.intervention = intervention
         self.factor = factor
+        self.interim_target = interim_target or InterimTargetDummy()
 
     def step(self) -> None:
-        self.reward = self.factor * self.intervention.target.reached
+        target_reached = self.intervention.target.reached or self.interim_target.reached
+        self.reward = self.factor * target_reached
 
     def reset(self, episode_nr: int = 0) -> None:
         self.reward = 0.0
