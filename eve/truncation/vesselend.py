324475494244843df750d01230b3aab6bcb3251b
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2023-06-02 08:44:40 +0200

    trunc.vesselend: bugfix

--- eve/truncation/vesselend.py
+++ eve/truncation/vesselend.py
@@ -8,7 +8,7 @@ class VesselEnd(Truncation):
 
     @property
     def truncated(self) -> bool:
-        tip = self.intervention.simulation.instrument_position_vessel_cs[0]
+        tip = self.intervention.simulation.dof_positions[0]
         return self.intervention.vessel_tree.at_tree_end(tip)
 
     def step(self) -> None:
