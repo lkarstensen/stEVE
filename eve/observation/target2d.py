a3af3e2eeae2e60c98d0baf563674ac0bc3eacb9
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2023-06-02 08:42:53 +0200

    obs.target2d: implemented interimtargets as optional

--- eve/observation/target2d.py
+++ eve/observation/target2d.py
@@ -1,17 +1,21 @@
+from typing import Optional
 import numpy as np
 
 from .observation import Observation, gym
 from ..intervention import Intervention
+from ..interimtarget import InterimTarget
 
 
 class Target2D(Observation):
     def __init__(
         self,
         intervention: Intervention,
+        interim_target: Optional[InterimTarget] = None,
         name: str = "target2d",
     ) -> None:
         self.name = name
         self.intervention = intervention
+        self.interim_target = interim_target
         self.obs = None
 
     @property
@@ -19,7 +23,15 @@ class Target2D(Observation):
         return self.intervention.fluoroscopy.tracking2d_space
 
     def step(self) -> None:
-        self.obs = np.array(self.intervention.target.coordinates2d, dtype=np.float32)
+        if (
+            self.interim_target is not None
+            and self.interim_target.coordinates2d is not None
+        ):
+            self.obs = np.array(self.interim_target.coordinates2d, dtype=np.float32)
+        else:
+            self.obs = np.array(
+                self.intervention.target.coordinates2d, dtype=np.float32
+            )
 
     def reset(self, episode_nr: int = 0) -> None:
         self.step()
