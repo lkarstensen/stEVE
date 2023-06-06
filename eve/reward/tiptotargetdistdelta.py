6fcdd6394e926ec16a08e4a78976e932331c30e9
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2023-06-06 13:05:02 +0200

    reward.tiptotargetdistdelta: implemented interim_target

--- eve/reward/tiptotargetdistdelta.py
+++ eve/reward/tiptotargetdistdelta.py
@@ -1,19 +1,31 @@
+from typing import Optional
 import numpy as np
 
 from .reward import Reward
 from ..intervention import Intervention
+from ..interimtarget import InterimTarget
 
 
 class TipToTargetDistDelta(Reward):
-    def __init__(self, intervention: Intervention, factor: float) -> None:
-        self.intervention = intervention
+    def __init__(
+        self,
+        factor: float,
+        intervention: Intervention,
+        interim_target: Optional[InterimTarget],
+    ) -> None:
         self.factor = factor
+        self.intervention = intervention
+        self.interim_target = interim_target
         self._last_dist = None
         self._last_target = None
 
     def step(self) -> None:
         tip = self.intervention.fluoroscopy.tracking3d[0]
-        target = self.intervention.target.coordinates3d
+        if self.interim_target.coordinates3d is not None:
+            target = self.interim_target.coordinates3d
+        else:
+            target = self.intervention.target.coordinates3d
+
         dist = np.linalg.norm(tip - target)
 
         dist_delta = dist - self._last_dist
