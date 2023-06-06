9512bac8c6acec81b891419c82c367484651644b
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2023-06-06 22:48:42 +0200

    reward.targetreached: implemented flag for final target only after interim target

--- eve/reward/targetreached.py
+++ eve/reward/targetreached.py
@@ -10,13 +10,23 @@ class TargetReached(Reward):
         intervention: Intervention,
         factor: float,
         interim_target: Optional[InterimTarget] = None,
+        final_only_after_all_interim: bool = True,
     ) -> None:
         self.intervention = intervention
         self.factor = factor
         self.interim_target = interim_target or InterimTargetDummy()
+        self.final_only_after_all_interim = final_only_after_all_interim
 
     def step(self) -> None:
-        target_reached = self.intervention.target.reached or self.interim_target.reached
+        if self.final_only_after_all_interim:
+            if self.interim_target.coordinates3d is None:
+                target_reached = self.intervention.target.reached
+            else:
+                target_reached = self.interim_target.reached
+        else:
+            target_reached = (
+                self.intervention.target.reached or self.interim_target.reached
+            )
         self.reward = self.factor * target_reached
 
     def reset(self, episode_nr: int = 0) -> None:
