4557dd89f8c1fccb90bb7d5d34f1f7678746c823
benjamin-m-jackson <benjamin.m.jackson@kcl.ac.uk>
2023-11-22 16:49:42 +0000

    add serial implementation

--- eve/util/userinput/instrumentaction.py
+++ eve/util/userinput/instrumentaction.py
@@ -2,6 +2,8 @@
 from typing import Tuple
 import numpy as np
 import pygame
+import serial
+import time
 
 
 class JoyOneDevice:
@@ -30,13 +32,13 @@ class KeyboardOneDevice:
         rot = 0.0
         pygame.event.get()
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_UP]:
+        if keys_pressed[pygame.K_w] or keys_pressed[pygame.K_UP]:
             trans += self.actions[0]
-        if keys_pressed[pygame.K_DOWN]:
+        if keys_pressed[pygame.K_s] or keys_pressed[pygame.K_DOWN]:
             trans -= self.actions[0]
-        if keys_pressed[pygame.K_LEFT]:
+        if keys_pressed[pygame.K_a] or keys_pressed[pygame.K_LEFT]:
             rot += self.actions[1]
-        if keys_pressed[pygame.K_RIGHT]:
+        if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_RIGHT]:
             rot -= self.actions[1]
         return np.array((trans, rot))
 
@@ -77,3 +79,161 @@ class KeyboardTwoDevice:
         if keys_pressed[pygame.K_j]:
             rot1 -= self.actions[1][1]
         return np.array(((trans0, rot0), (trans1, rot1)))
+
+
+class SerialOneDevice:
+    
+    def __init__(self, com:str, baud:int) -> None:
+        pygame.init()
+        self.com = com
+        self.baud = baud
+        self.ser = self.haptic_serial_open()
+        self.old_trans = 0
+        self.old_rot = 0
+
+
+    def get_action(self):
+
+        haptic_feedback_set = [ 0 , 0 ]
+        trans = 0.0
+        rot = 0.0
+
+        haptic_pos , haptic_vel, haptic_force_get = self.haptic_interface( self.ser , haptic_feedback_set )
+        pygame.event.get()
+
+        print(haptic_pos)
+
+        trans = haptic_pos[0] - self.old_trans
+        rot = haptic_pos[1] - self.old_rot
+
+        self.old_trans = haptic_pos[0]
+        self.old_rot = haptic_pos[1]
+
+        return np.array((trans, rot))
+
+    def haptic_serial_open(self):    
+        # open serial
+        self.ser = serial.Serial('COM13', 115200, timeout=1)
+        time.sleep(1)
+        return self.ser
+
+    def haptic_interface(self, ser , haptic_feedback_set ):
+        haptic_pos = [ 0, 0, 0, 0, 0, 0 ]
+        haptic_vel = [ 0, 0, 0, 0, 0, 0 ]
+        haptic_force_get = [0, 0]
+
+        # send serial inputs
+        # ser.write(bytes("10,10\n", 'utf-8'))
+        self.ser.write( bytes( str( haptic_feedback_set[0] ) + ',' + str( haptic_feedback_set[1] ) + '\n' , 'utf-8') )
+        time.sleep(0.01)
+
+        isWaiting = ser.inWaiting()
+        if ( isWaiting > 0):
+            # print( ser.read(ser.inWaiting()) )
+
+            try:
+                data_str = ser.read(ser.inWaiting()).decode('ascii')
+            except:
+                return haptic_pos , haptic_vel , haptic_force_get
+            # print(data_str, end='\n')
+            time.sleep(0.01)
+
+            haptic_encoder = data_str.split('\r\n')
+            he_size = len( haptic_encoder )
+
+            while( he_size > 2 ): # looking for valid data
+                he_split = haptic_encoder[he_size-2].split(',') # skip the last entry since it is usually incomplete!
+                if len( he_split ) == 14:
+                    lin_wire, rot_wire, lin_mcath, rot_mcath, lin_cath, rot_cath, vlin_wire, vrot_wire, vlin_mcath, vrot_mcath, vlin_cath, vrot_cath, lin_pwm, rot_pwm = haptic_encoder[he_size-2].split(',')
+                    haptic_pos = [ int(lin_wire), int(rot_wire), int(lin_mcath), int(rot_mcath), int(lin_cath), int(rot_cath) ]
+                    haptic_vel = [ int(vlin_wire), int(vrot_wire), int(vlin_mcath), int(vrot_mcath), int(vlin_cath), int(vrot_cath) ]
+                    haptic_force_get = [ int(lin_pwm), int(rot_pwm) ]
+                    # print( he_split ) # test
+                    break
+                he_size = he_size - 1
+
+        return haptic_pos , haptic_vel , haptic_force_get
+    
+class SerialTwoDevice:
+    
+    def __init__(self, com:str, baud:int) -> None:
+        pygame.init()
+        self.actions = actions
+        self.com = com
+        self.baud = baud
+        self.ser = self.haptic_serial_open()
+
+        self.old_trans0 = 0
+        self.old_rot0 = 0
+        self.old_trans1 = 0
+        self.old_rot1 = 0
+
+
+
+    def get_action(self):
+
+        haptic_feedback_set = [ 0 , 0 ]
+        trans0 = 0.0
+        rot0 = 0.0
+        trans1 = 0.0
+        rot1 = 0.0
+
+        haptic_pos , haptic_vel, haptic_force_get = self.haptic_interface( self.ser , haptic_feedback_set )
+        pygame.event.get()
+
+        print(haptic_pos)
+
+        trans0 = haptic_pos[0] - self.old_trans0
+        rot0 = haptic_pos[1] - self.old_rot0
+        trans1 = haptic_pos[2] - self.old_trans1
+        rot1 = haptic_pos[3] - self.old_rot1
+
+        self.old_trans0 = haptic_pos[0]
+        self.old_rot0 = haptic_pos[1]
+        self.old_trans1 = haptic_pos[2]
+        self.old_rot1 = haptic_pos[3]
+
+        return np.array((trans0, rot0), (trans1, rot1))
+
+    def haptic_serial_open(self):    
+        # open serial
+        self.ser = serial.Serial(self.COM, self.baud, timeout=1)
+        time.sleep(1)
+        return self.ser
+
+    def haptic_interface(self, ser , haptic_feedback_set ):
+        haptic_pos = [ 0, 0, 0, 0, 0, 0 ]
+        haptic_vel = [ 0, 0, 0, 0, 0, 0 ]
+        haptic_force_get = [0, 0]
+
+        # send serial inputs
+        # ser.write(bytes("10,10\n", 'utf-8'))
+        self.ser.write( bytes( str( haptic_feedback_set[0] ) + ',' + str( haptic_feedback_set[1] ) + '\n' , 'utf-8') )
+        time.sleep(0.01)
+
+        isWaiting = ser.inWaiting()
+        if ( isWaiting > 0):
+            # print( ser.read(ser.inWaiting()) )
+
+            try:
+                data_str = ser.read(ser.inWaiting()).decode('ascii')
+            except:
+                return haptic_pos , haptic_vel , haptic_force_get
+            # print(data_str, end='\n')
+            time.sleep(0.01)
+
+            haptic_encoder = data_str.split('\r\n')
+            he_size = len( haptic_encoder )
+
+            while( he_size > 2 ): # looking for valid data
+                he_split = haptic_encoder[he_size-2].split(',') # skip the last entry since it is usually incomplete!
+                if len( he_split ) == 14:
+                    lin_wire, rot_wire, lin_mcath, rot_mcath, lin_cath, rot_cath, vlin_wire, vrot_wire, vlin_mcath, vrot_mcath, vlin_cath, vrot_cath, lin_pwm, rot_pwm = haptic_encoder[he_size-2].split(',')
+                    haptic_pos = [ int(lin_wire), int(rot_wire), int(lin_mcath), int(rot_mcath), int(lin_cath), int(rot_cath) ]
+                    haptic_vel = [ int(vlin_wire), int(vrot_wire), int(vlin_mcath), int(vrot_mcath), int(vlin_cath), int(vrot_cath) ]
+                    haptic_force_get = [ int(lin_pwm), int(rot_pwm) ]
+                    # print( he_split ) # test
+                    break
+                he_size = he_size - 1
+
+        return haptic_pos , haptic_vel , haptic_force_get
